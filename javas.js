f649a849b3bf452b04d748a027d263868411e235
agcode <cheri.geor@gmail.com>
2015-10-19 13:29:32 +0530

    First commit

---
+++ javas.js
@@ -0,0 +1,6 @@
+function go(){
+	document.getElementById("span2").style.color="green";
+	document.getElementById("span2").style.fontSize="200%";
+	alert("Thank you for clicking on the image ! This alert box is triggered using javascript");
+	
+}
